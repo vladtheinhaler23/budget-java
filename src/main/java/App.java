e7c0494f10b41ee27e2f1f663b8c58176eb67a06
Elissa Sosland and Joshua Logan <elissa+joshua@>
2016-05-16 15:22:11 -0700

    add templates and changes to front end, need to fix integration tests for useriscreatedtest and transactionsareaddedanddisplayed test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,5 +9,42 @@ public class App {
     staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("users", User.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/users/:id", (request, response) -> {
+      String name = request.queryParams("name");
+      int budget = Integer.parseInt(request.queryParams("budget"));
+      User newUser = new User(name, budget);
+      newUser.save();
+      response.redirect("/users/" + newUser.getId());
+      return null;
+    });
+
+    get("/user/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("transactions", Transaction.all());
+      User user = User.find(Integer.parseInt(request.params(":id")));
+      model.put("user", user);
+      model.put("template", "templates/user.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/user/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      User user = User.find(Integer.parseInt(request.params(":id")));
+      int amount = Integer.parseInt(request.queryParams("amount"));
+      Transaction newTransaction = new Transaction(amount, user.getId());
+      newTransaction.save();
+      model.put("user", user);
+      model.put("transactions", Transaction.all());
+      model.put("template", "templates/user.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -15,4 +15,46 @@ public class AppTest extends FluentTest {
   public WebDriver getDefaultDriver() {
     return webDriver;
   }
+
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("BUDget");
+    }
+
+    @Test
+    public void userIsCreatedTest() {
+      goTo("http://localhost:4567/");
+      fill("#name").with("Fozzie Bear");
+      submit(".btn");
+      assertThat(pageSource()).contains("Fozzie Bear");
+    }
+
+    @Test
+    public void userPageIsDisplayedTest() {
+      User myUser = new User("Fozzie Bear", 200);
+      myUser.save();
+      String userPath = String.format("http://localhost:4567/user/%d", myUser.getId());
+      goTo(userPath);
+      assertThat(pageSource()).contains("Fozzie Bear");
+    }
+
+    @Test
+    public void transactionsAreAddedAndDisplayed() {
+      goTo("http://localhost:4567/");
+      fill("#name").with("Fozzie Bear");
+      submit(".btn");
+      fill("#amount").with("500");
+      submit(".btn");
+      assertThat(pageSource()).contains("500");
+    }
+
+
 }
